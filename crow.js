cc96a8317d724b74072378632f234147f7d84cf3
Eva <evauwu@riseup.net>
2020-11-29 19:38:12 +0100

    Beeps

--- crow.js
+++ crow.js
@@ -28,8 +28,38 @@ document.addEventListener("DOMContentLoaded", e => {
   english.addEventListener("input", e => encoded.value = crow(english.value));
   encoded.addEventListener("input", e => english.value = crow(encoded.value, true));
 
-  const synth = new Tone.Synth().toDestination();
-  document.querySelectorAll("button").forEach(b => b.addEventListener("mousedown", e => synth.triggerAttackRelease(b.innerText.split("\n")[2], "8n")));
+  const sampler = new Tone.Sampler({
+    urls: {
+      "C3": "C3.opus",
+      "C#3": "Csharp3.opus",
+      "D3": "D3.opus",
+      "D#3": "Dsharp3.opus",
+      "E3": "E3.opus",
+      "F3": "F3.opus",
+      "F#3": "Fsharp3.opus",
+      "G3": "G3.opus",
+      "G#3": "Gsharp3.opus",
+      "A3": "A3.opus",
+      "A#3": "Asharp3.opus",
+      "B3": "B3.opus",
+      "C4": "C4.opus",
+      "C#4": "Csharp4.opus",
+      "D4": "D4.opus",
+      "D#4": "Dsharp4.opus",
+      "E4": "E4.opus",
+      "F4": "F4.opus",
+      "F#4": "Fsharp4.opus",
+      "G4": "G4.opus",
+      "G#4": "Gsharp4.opus",
+      "A4": "A4.opus",
+      "A#4": "Asharp4.opus",
+      "B4": "B4.opus",
+      "C5": "C5.opus",
+      "C#5": "Csharp5.opus"
+    },
+    baseUrl: "beeps/"
+  }).toDestination();
+  document.querySelectorAll("button").forEach(b => b.addEventListener("mousedown", e => sampler.triggerAttackRelease(b.innerText.split("\n")[2], "8n")));
 
   document.querySelectorAll("th").forEach(h => {if (h.innerText) h.addEventListener("click", e => {
     document.querySelectorAll(".yselected").forEach(o => o.classList.remove("yselected"));
